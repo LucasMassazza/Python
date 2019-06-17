b897afc77b5b5eb69c0277ed4ef19bede33e3e58
Lucas <lucasmassazza@gmail.com>
2019-06-17 16:45:35 -0300

    Actualizando

--- TrabajoPracticoSubReserva/Inicio.py
+++ TrabajoPracticoSubReserva/Inicio.py
@@ -20,14 +20,12 @@ def main():
     cierrePrograma = False
     cierreMenu = False
     seleccionOpcionMenu = 0
-
-    tituloYSaludo()
-
+    
     while cierrePrograma != True:
 
         os.system("cls")
         tituloYSaludo()
-        print ("1. Ingresar Datos\n2. Estadisticas\n0. Salir")
+        print ("1. Ingresar Datos\n2. Estadisticas\n0. Salir\n\n")
         seleccionOpcionMenu = input()
         if seleccionOpcionMenu == 1:
            menu.menu()
@@ -36,15 +34,15 @@ def main():
         else:
            sys.exit()
 
-
+def tituloYSaludo():
+    print ("\033[1;36m")
+    print ("BIENVENIDOS A CHAU BIONDI").center(50,"=")
+    print ("")
+    print ("\033[1;31m""SISTEMA DE RESERVA""\033[0m").center(60)
+    print ("")                                                      
 
 main()
 
 
-def tituloYSaludo():
-    print ("\033[1;36m")
-    print ("BIENVENIDOS A CHAU BIONDI").center(50, "=")
-    print ("")
-    print ("\033[1;31m""SISTEMA DE SUB RESERVA""\033[0m").center(60)
-    print ("")
+
 
