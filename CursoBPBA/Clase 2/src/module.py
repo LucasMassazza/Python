d5e05698096a16c41b321bd013973e6600c4a26a
Massazza Lucas <lucasmassazza@gmail.com>
2023-05-23 17:19:28 -0300

    modified:   "CursoBPBA/Clase 1/Clase 1 - Fundamentos de programaci\303\263n en Python - ejercicios.ipynb" 	modified:   "CursoBPBA/Clase 1/Clase 1 - Fundamentos de programaci\303\263n en Python.ipynb" 	new file:   CursoBPBA/Clase 2/.DS_Store 	new file:   CursoBPBA/Clase 2/Clase 2 - Funciones, clases y objetos.ipynb 	new file:   CursoBPBA/Clase 2/Clase 2 - ejercicios.ipynb 	new file:   CursoBPBA/Clase 2/img/namespace.png 	new file:   CursoBPBA/Clase 2/src/.DS_Store 	new file:   CursoBPBA/Clase 2/src/module.py

---
+++ CursoBPBA/Clase 2/src/module.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+
+print("Este es el modulo 'module.py'")
+
+a = 10
+
+b = 20
+
+
+def f1(x, y):
+    print("Esta función se encuentra en el modulo 'module'")
+    return x, y
+
+
+# Suma de dos números.
+def suma(a, b):
+    return a + b
+
+
+# Resta de dos números.
+def resta(a, b):
+    return a - b
+
+
+# Multiplicación de un número por dos.
+def doble(a):
+    return a*2
