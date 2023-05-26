c96abedb992d5dc7a3404858f1270ea1e4f71979
Massazza Lucas <lucasmassazza@gmail.com>
2023-05-26 19:29:03 -0300

    modified:   "CursoBPBA/Clase 1/Clase 1 - Fundamentos de programaci\303\263n en Python - ejercicios.ipynb" 	deleted:    CursoBPBA/Clase 1/ProbarFunciones.py 	new file:   CursoBPBA/Clase 1/Solucion.py 	new file:   CursoBPBA/Clase 1/__pycache__/Solucion.cpython-311.pyc

---
+++ CursoBPBA/Clase 1/Solucion.py
@@ -0,0 +1,48 @@
+def Ejercicio1(n1,n2):
+
+    producto=n1*n2
+    
+
+    if producto>100:
+        print(f"El producto de {n1} y {n2} es: {producto}")
+    else:
+        suma=n1+n2
+        print(f"El producto de {n1} y {n2} es: {producto}\nLa suma es {suma}")
+    
+def Ejercicio2(Lista):
+
+    ListaSum = []
+    Indice = 0
+
+
+    for vocales in range(0, len(Lista)-1):
+        
+        IndGuia = Indice+1
+        ListaSum.append(Lista[Indice]+Lista[IndGuia])
+        Indice = Indice+1 
+
+    ListaSum.sort()
+    print(ListaSum.sort)
+
+def Ejercicio3(oracion):
+    vocales = ['a','e','i','o','u']
+    VocalesMay =[]
+    for char in oracion:
+        if char in vocales:
+            VocalesMay.append(char.upper())
+
+def Ejercicio4(numeros, divisor):
+    
+    multiplos = [multiplo for multiplo in numeros if multiplo % divisor == 0]
+    multiplosFiltrador = []
+
+    for x in range(len(multiplos)):
+        if multiplos[x] not in multiplosFiltrador:
+            multiplosFiltrador.append(multiplos[x])
+
+    print(multiplos)
+    multiplosFiltrador.sort()
+    multiplosFiltrador
+
+
+
